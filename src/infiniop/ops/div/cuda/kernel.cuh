55bcced6b73100e7146b4441cd4b1adba5553bec
mxCynic <mx1545814198@gmail.com>
2025-08-07 16:09:17 +0800

    fix: div floor error at bf16

--- src/infiniop/ops/div/cuda/kernel.cuh
+++ src/infiniop/ops/div/cuda/kernel.cuh
@@ -45,9 +45,16 @@ public:
     template <typename T>
     __device__ __forceinline__ T operator()(const T &a, const T &b) const {
         if constexpr (std::is_same_v<T, half>) {
-            return hfloor(__hdiv(a, b));
+            float fa = __half2float(a);
+            float fb = __half2float(b);
+            float res = floorf(fdividef(a, b));
+            return __float2half(res);
         } else if constexpr (std::is_same_v<T, cuda_bfloat16>) {
-            return hfloor(__hdiv(a, b));
+            float fa = __bfloat162float(a);
+            float fb = __bfloat162float(b);
+            float res = floorf(fdividef(a, b));
+            return __float2bfloat16(res);
+
         } else if constexpr (std::is_same_v<T, float>) {
             return floorf(fdividef(a, b));
         } else if constexpr (std::is_same_v<T, double>) {
