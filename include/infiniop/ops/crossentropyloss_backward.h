3a8dfaf0ab88d80ba3d5329200c1edafacaba515
mxCynic <mx1545814198@gmail.com>
2025-08-07 16:09:17 +0800

    refactor: variables name

--- include/infiniop/ops/crossentropyloss_backward.h
+++ include/infiniop/ops/crossentropyloss_backward.h
@@ -1,5 +1,5 @@
-#ifndef __INFINIOP_CROSSENTROPYLOSSBACKWARD_API_H__
-#define __INFINIOP_CROSSENTROPYLOSSBACKWARD_API_H__
+#ifndef __INFINIOP_CROSSENTROPYLOSS_BACKWARD_API_H__
+#define __INFINIOP_CROSSENTROPYLOSS_BACKWARD_API_H__
 
 #include "../operator_descriptor.h"
 
@@ -7,18 +7,18 @@ typedef struct InfiniopDescriptor *infiniopCrossEntropyLossBackWardDescriptor_t;
 
 __C __export infiniStatus_t infiniopCreateCrossEntropyLossBackWardDescriptor(infiniopHandle_t handle,
                                                                              infiniopCrossEntropyLossBackWardDescriptor_t *desc_ptr,
-                                                                             infiniopTensorDescriptor_t c,
-                                                                             infiniopTensorDescriptor_t a,
-                                                                             infiniopTensorDescriptor_t b);
+                                                                             infiniopTensorDescriptor_t grad_logits,
+                                                                             infiniopTensorDescriptor_t probs,
+                                                                             infiniopTensorDescriptor_t target);
 
 __C __export infiniStatus_t infiniopGetCrossEntropyLossBackWardWorkspaceSize(infiniopCrossEntropyLossBackWardDescriptor_t desc, size_t *size);
 
 __C __export infiniStatus_t infiniopCrossEntropyLossBackWard(infiniopCrossEntropyLossBackWardDescriptor_t desc,
                                                              void *workspace,
                                                              size_t workspace_size,
-                                                             void *c,
-                                                             const void *a,
-                                                             const void *b,
+                                                             void *grad_logits,
+                                                             const void *probs,
+                                                             const void *target,
                                                              void *stream);
 
 __C __export infiniStatus_t infiniopDestroyCrossEntropyLossBackWardDescriptor(infiniopCrossEntropyLossBackWardDescriptor_t desc);
